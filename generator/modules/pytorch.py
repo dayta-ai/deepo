68a7c5255f1553efad4ffd1cc449e3bcecc0f830
conraddd <conradhch@hotmail.com>
2019-08-14 12:15:30 +0800

    modify pytorch and torchvision

--- generator/modules/pytorch.py
+++ generator/modules/pytorch.py
@@ -4,7 +4,7 @@ from .python import Python
 
 
 @dependency(Python)
-@version('1.1')
+@version('1.2')
 @source('pip')
 class Pytorch(Module):
 
@@ -12,21 +12,15 @@ class Pytorch(Module):
         py_ver = int(float(self.composer.ver(Python))*10)
         if py_ver not in [27, 35, 36, 37]:
             raise NotImplementedError('unsupported python version for pytorch')
-        if self.version not in ['1.1']:
+        if self.version not in ['1.1', '1.2']:
             raise NotImplementedError('unsupported pytorch version')
 
         cuver = 'cpu' if self.composer.cuda_ver is None else 'cu%d' % (
             float(self.composer.cuda_ver) * 10)
+        if float(self.version) == 1.1:
+            torchvisionver = 0.3
+        else:
+            torchvisionver = 0.4
         return r'''
-            $PIP_INSTALL \
-                future \
-                numpy \
-                protobuf \
-                enum34 \
-                pyyaml \
-                typing \
-            	torch \
-                && \
-            $PIP_INSTALL https://download.pytorch.org/whl/{cuver}/torch-{torchver}.0-cp{pyver}-cp{pyver}m-linux_x86_64.whl && \
-            $PIP_INSTALL https://download.pytorch.org/whl/{cuver}/torchvision-0.3.0-cp{pyver}-cp{pyver}m-linux_x86_64.whl && \
-        '''.format(cuver=cuver, pyver=py_ver, torchver=self.version)
+            $PIP_INSTALL torch=={torchver} torchvision=={torchvisionver} && \
+        '''.format(torchver=self.version, torchvisionver=torchvisionver)
